e01cdcae2a97f494cc9254de475dd44e5bf51c0e
Ahmad Adil <ahmadadil950@gmail.com>
2022-09-08 21:37:43 -0400

    removed other acc calls

--- ray.cpp
+++ ray.cpp
@@ -83,10 +83,9 @@ hittable_list random_scene()
     auto ground_material = make_shared<lambertian>(color(0.5,0.5,0.5));
 
     world.add(make_shared<sphere>(point3(0,-1000.0,0), 1000.0, ground_material));
-    #pragma acc parallel loop gang worker
+    #pragma acc kernals
     for(int x = -11; x < 11; x++)
     {
-        #pragma acc loop vector
         for(int y = -11; y < 11; y++)
         {
             auto choose_mat = random_double();
